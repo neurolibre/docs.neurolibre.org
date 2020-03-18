c7e96aaf7afd5d434ae82fa495940df4286efc63
pbellec <pierre.bellec@gmail.com>
2020-03-17 23:53:03 -0400

    Fix: tell readthedocs not to search for contents.rst

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ author = 'The NeuroLibre Team'
 
 # The full version, including alpha/beta/rc tags
 release = 'v0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
